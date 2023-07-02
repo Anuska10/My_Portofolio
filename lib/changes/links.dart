8f82e0e33f4a9ba0af8aeeca68b3e847db81b480
Anuska10 <itsonlyanu10@gmail.com>
2023-07-02 19:04:31 +0530

    My Site

---
+++ lib/changes/links.dart
@@ -0,0 +1,17 @@
+// CV
+const String resume =
+    'https://drive.google.com/file/d/1KrSit2s5fUtQEP3NH6xtyzONc-ABONfn/view?usp=sharing';
+
+// Github Link
+const String gitHub = 'https://github.com/Anuska10';
+
+const String facebook =
+    'https://www.facebook.com/profile.php?id=100076532836600';
+
+const String twitter = 'https://twitter.com/Anuska1503';
+
+const String linkedin = 'https://www.linkedin.com/in/anuska-paul-84b48b261/';
+
+const String instagram = 'https://www.instagram.com/my_abstract_gallery/';
+
+
---
+++ lib/changes/strings.dart
@@ -0,0 +1,28 @@
+String hellotag = '''Hi there, Welcome to My World  ''';
+
+String yourname = "I'm Anuska Paul,";
+// String hometitle2 = "A Mobile Application\nDeveloper";
+
+String animationtxt1 = " Mobile App Developer";
+String animationtxt2 = " UI Designer";
+String animationtxt3 = " Web Developer";
+
+//
+String contactHeadding = "Let’s try my service now!";
+String contactSubHeadding =
+    "Let’s work together and make everything super beneficial and super useful.";
+//
+String miniDescription =
+    "Software Developer providing the Best Programming services and UI designs. Join me down below and let's get started!";
+
+String servicesSubHeading =
+    "I am beginning my journey as a Developer,Eager to delve into Flutter app and web development, despite lacking experience.Trying my best to flourish my skills.";
+//
+
+String protfolioSubHeading =
+    "I am beginning my journey as a Developer,Eager to delve into Flutter app and web development, despite lacking experience.Trying my best to flourish my skills.";
+const String aboutMeHeadline =
+    'I\'m ANUSKA PAUL, a Mobile App developer, Web developer and UI designer.';
+
+const String aboutMeDetail =
+    'I\'m currently pursuing my Btech degree in CSE from Narula Institute of Technology. I have been developing mobile apps for over 4 years now. I have worked in teams for various startups and helped them in launching their prototypes and got valuable learning experience. Been a CEO/Co-Founder of Onyxsio, Sri Lanka. Currently working as an Associate Software Engineer at Onyxsio.';
